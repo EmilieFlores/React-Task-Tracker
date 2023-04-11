f2b2f223dc7fc0499c1375f7a9d98f1a59d2de52
EmilieFlores <codingtime0000@gmail.com>
2023-04-10 19:13:50 -0500

    update

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import Header from './components/Header'
 const App = () => {
   return (
     <div className="container">
-      <Header /> 
+      <Header title='Hello'/> 
       
     </div>
-  )
+  );
 }
 
 export default App;
