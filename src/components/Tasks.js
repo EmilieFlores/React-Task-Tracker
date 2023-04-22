f917ac7670b2f4d3907c34e2dbf8a45110ed1b18
EmilieFlores <codingtime0000@gmail.com>
2023-04-22 11:06:48 -0500

    update

---
+++ src/components/Tasks.js
@@ -0,0 +1,28 @@
+const tasks = [
+    {
+        "id": 1,
+        "text": "test",
+        "other": "no-show",
+        "day": "monday",
+        "reminder": true
+      },
+      {
+        "text": "Test4",
+        "day": "Wednesday",
+        "reminder": false,
+        "id": 3
+      },
+      {
+        "text": "Task6",
+        "day": "Thursday",
+        "reminder": true,
+        "id": 4
+      }
+]
+const Tasks = () => {
+  return (
+    <div>Tasks</div>
+  )
+}
+
+export default Tasks
